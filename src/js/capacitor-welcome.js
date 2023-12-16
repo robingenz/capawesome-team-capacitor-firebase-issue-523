bcb1797229a00ee1b0f4f06cbc516d48ea3ac120
Daniil Kudryavtsev <danilalomaster@gmail.com>
2023-12-16 11:42:13 +0100

    add failing example

--- src/js/capacitor-welcome.js
+++ src/js/capacitor-welcome.js
@@ -1,6 +1,49 @@
 import { Browser } from "@capacitor/browser";
 import { SplashScreen } from "@capacitor/splash-screen";
 
+import { initializeApp } from 'firebase/app';
+import { initializeFirestore } from 'firebase/firestore';
+import { FirebaseFirestore } from '@capacitor-firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBJ5yTxOrUByDdwfxSZ9s-CkYeCm_rwcLw",
+    authDomain: "test-a8fc5.firebaseapp.com",
+    projectId: "test-a8fc5",
+    storageBucket: "test-a8fc5.appspot.com",
+    messagingSenderId: "192576538022",
+    appId: "1:192576538022:web:00f640ad899a44618b7b67"
+};
+
+async function init(){
+
+    const thisCodeFails = async () => {
+        const reference = `users/2NkGwKiBHFV1K5kDg9fJOcig78u2/lessons/lesson_1`;
+        const { snapshot: doc1 } = await FirebaseFirestore.getDocument({
+            reference,
+        });
+
+        if(!doc1.data){
+            await FirebaseFirestore.setDocument({
+                reference,
+                data: { a:1, b: 2 },
+            });
+        }
+    };
+
+    const firebaseApp = initializeApp(firebaseConfig);
+    const db = initializeFirestore(firebaseApp, {});
+
+    try{
+        await thisCodeFails();
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+// init();
+
+
 window.customElements.define(
   "capacitor-welcome",
   class extends HTMLElement {
